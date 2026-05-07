4e642091f3a285be37750cb40470b51db8b55dad
Rithesh <ritheshasowda@gmail.com>
2026-05-07 21:32:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,3 @@ from ranking import rank_resumes
 st.title("AI Resume Screening")
 
 job_desc = st.text_area("Paste Job Description")
-
-uploaded_files = st.file_uploader(
-    "Upload Resumes", type=["pdf"], accept_multiple_files=True
-)
-
-if st.button("Analyze"):
-    results = rank_resumes(uploaded_files, job_desc)
-    st.write(results)
