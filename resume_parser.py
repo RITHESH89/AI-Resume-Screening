6f0936a6deaa70c19cb94ca627c4c74efb5be1f4
Rithesh <ritheshasowda@gmail.com>
2026-05-22 21:38:49 +0530

    Update resume_parser.py

--- resume_parser.py
+++ resume_parser.py
@@ -2,7 +2,4 @@ from pypdf import PdfReader
 
 def extract_text(file):
     reader = PdfReader(file)
-    text = ""
-    for page in reader.pages:
-        text += page.extract_text()
-    return text
+ 
